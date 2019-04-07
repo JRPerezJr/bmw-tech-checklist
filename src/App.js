3be05a75d3d5d12d61bd3ebfe42bedc8fc1c0030
Juan P <pront23@gmail.com>
2019-04-07 17:03:58 +0900

    First commit app basic

--- src/App.js
+++ src/App.js
@@ -1,28 +1,155 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+function Todo({ todo, index, completeTodo, removeTodo }) {
+  return <div
+            style={{textDecoration: todo.isCompleted ? 'line-through red' : ''}}
+            className="todo"
           >
-            Learn React
-          </a>
-        </header>
+          { todo.text }
+          <div>
+            <button
+              onClick={() => completeTodo(index)}
+            >
+            Complete
+            </button>
+            <button
+              onClick={() => removeTodo(index)}
+            >
+            X
+            </button>
+          </div>
+        </div>;
+}
+function TodoForm({addTodo}) {
+   const [value, setValue] = useState('');
+
+   const handleSubmit = e => {
+     e.preventDefault();
+     if(!value) return;
+     addTodo(value);
+     setValue('');
+   }
+   return (
+     <form onSubmit={handleSubmit}>
+      <input type="text" className="input" placeholder="Add to technician todo list" value={value} onChange={e => setValue (e.target.value)}/>
+     </form>
+   )
+ }
+ 
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: 'Verify the lift area is clear of objects and people',
+      isCompleted: false
+    },
+    {
+      text: 'Drive vehicle on to lift',
+      isCompleted: false
+    },
+    {
+      text: 'Perform walk around inspection',
+      isCompleted: false
+    },
+    {
+      text: 'Verify vehicle in-service date',
+      isCompleted: false
+    },
+    {
+      text: 'Check vehicle title status',
+      isCompleted: false
+    },
+    {
+      text: 'Verify vehicle is not reported as stolen',
+      isCompleted: false
+    },
+    {
+      text: 'Check for open campaigns',
+      isCompleted: false
+    },
+    {
+      text: 'Connect ICOM and perform vehicle test',
+      isCompleted: false
+    },
+    {
+      text: 'Open hood and perform visual inspection',
+      isCompleted: false
+    },
+    {
+      text: 'Read results of the vehicle test',
+      isCompleted: false
+    },
+    {
+      text: 'Determine if vehicle repairs will be covered under new or extended warranty',
+      isCompleted: false
+    },
+    {
+      text: 'Determine if vehicle repairs will be covered under the emissions warranty',
+      isCompleted: false
+    },
+    {
+      text: 'Continue with Test Plans if available',
+      isCompleted: false
+    },
+    {
+      text: 'Send request to the parts counter and verify parts availability',
+      isCompleted: false
+    },
+    {
+      text: 'Compile list of repairs with labor times and parts',
+      isCompleted: false
+    },
+    {
+      text: 'Report back results of recommended repairs to Service Advisor',
+      isCompleted: false
+    },
+    {
+      text: 'Park vehicle if parking is available',
+      isCompleted: false
+    },
+    {
+      text: 'Wait for authorization from Service Writer',
+      isCompleted: false
+    },
+    {
+      text: 'Drive in the next vehicle to repeat todo list',
+      isCompleted: false
+    }
+
+  ]);
+
+  const addTodo =text => {
+    const newTodos = [...todos, {text}];
+    setTodos(newTodos);
+  };
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
